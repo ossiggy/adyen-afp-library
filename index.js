7a7003b7d2e1e539ecf76cb81fd79cd0388229c8
Mike Ossig <mike.ossig@adyen.com>
2022-10-20 08:37:14 -0400

    index update

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-require('dotenv').config();
 const request = require('request-promise');
-
-const { API_KEY, PLATFORM_KEY } = process.env;
-const { base64Encode } = require("./resources/convertToBase.js");
-const base64 = base64Encode("./PASSED.jpg")
-
-
-const getNotiList = {
-  uri: "https://cal-test.adyen.com/cal/services/Notification/v6/getNotificationConfigurationList",
-  method: "POST",
-  headers: {
-    "Content-type": "application/json",
-    "x-api-key": PLATFORM_KEY
-  },
-  body: {},
-  json: true
-};
 
 const updateNoti = {
   uri: "https://cal-test.adyen.com/cal/services/Notification/v6/updateNotificationConfiguration",
@@ -34,39 +17,7 @@ const updateNoti = {
   json: true
 };
 
-const createAccountHolder = {
-  uri: "https://cal-test.adyen.com/cal/services/Account/v6/createAccountHolder",
-  method: "POST",
-  headers: {
-    "Content-type": "application/json",
-    "x-api-key": PLATFORM_KEY
-  },
-  body: {
-    "accountHolderCode": "TEST_BUSINESS_ACCOUNT",
-    "accountHolderDetails": {
-      "address": {
-          "country": "US"
-      },
-      "businessDetails": {
-        "legalBusinessName": "Real Good Restaurant Inc.",
-        "shareholders": [
-          {
-              "name": {
-                "firstName": "Maria",
-                "lastName": "Green"
-              },
-              "address": {
-                "country": "US"
-              }
-          }
-        ]
-      },
-      "email": "maria@green.com"
-    },
-    "legalEntity": "Business"
-  },
-   json: true 
-};
+
 
 
 const postRequest = async (options) => {
